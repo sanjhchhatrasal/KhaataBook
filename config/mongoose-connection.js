350a63ff45c835c5d9e31974cdaead9c1c95a6fd
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-07-27 13:51:56 +0530

    Database

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`${process.env.MONGO_DB_URI}/khaatabookPractice`)
+mongoose.connect(`${process.env.MONGO_DB_URI}`)
 
 const db = mongoose.connection;
 
